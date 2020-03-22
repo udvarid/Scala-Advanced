ee485690f0f5ddf791bc6c6c5ad0e99bb8e91589
udvari.donat <udvarid@hotmail.com>
2020-03-22 14:34:16 +0100

    Myset following 2

--- src/exercises/MySet.scala
+++ src/exercises/MySet.scala
@@ -58,6 +58,8 @@ class DonSet[A](head: A, tail: MySet[A] ) extends MySet[A] {
   override def --(anotherSet: MySet[A]): MySet[A] = filter(!anotherSet)
 
   override def &(anotherSet: MySet[A]): MySet[A] = filter(anotherSet)
+
+  override def unary_! : MySet[A] = new PropertyBasedSet[A](x => !this.contains(x))
 }
 
 class EmptySet[A] extends MySet[A] {
@@ -81,11 +83,38 @@ class EmptySet[A] extends MySet[A] {
 
   override def &(anotherSet: MySet[A]): MySet[A] = this
 
-  override def unary_! : MySet[A] = new AllInclusiveSet[A]
+  override def unary_! : MySet[A] = new PropertyBasedSet[A](_ => true)
 }
 
 
-class PropertyBasedSet[A](property: A => Boolean) extends MySet[A]
+class PropertyBasedSet[A](property: A => Boolean) extends MySet[A] {
+  override def contains(elem: A): Boolean = property(elem)
+
+  override def +(elem: A): MySet[A] =
+    new PropertyBasedSet[A](x => property(x) || x == elem)
+
+  override def ++(anotherSet: MySet[A]): MySet[A] =
+    new PropertyBasedSet[A](x => property(x) || anotherSet(x))
+
+  override def map[B](f: A => B): MySet[B] = politeFail
+
+  override def flatMap[B](f: A => MySet[B]): MySet[B] = politeFail
+
+  override def foreach(f: A => Unit): Unit = politeFail
+
+  override def filter(predicate: A => Boolean): MySet[A] =
+    new PropertyBasedSet[A](x=> property(x) && predicate(x))
+
+  override def -(elem: A): MySet[A] = filter(x => x != elem)
+
+  override def --(anotherSet: MySet[A]): MySet[A] = filter(!anotherSet)
+
+  override def &(anotherSet: MySet[A]): MySet[A] = filter(anotherSet)
+
+  override def unary_! : MySet[A] = new PropertyBasedSet[A](x => !property(x))
+
+  def politeFail = throw new IllegalArgumentException("Really deep rabbit hole")
+}
 
 object MySet {
   def apply[A](values: A*): MySet[A] = {
--- src/lectures/part2Advanced/FunctionalSet.scala
+++ src/lectures/part2Advanced/FunctionalSet.scala
@@ -21,6 +21,19 @@ object FunctionalSet extends App{
   val s6 = s5 filter (_ % 2 == 0)
   s6 foreach println
 
+  //testing the opposite set
+  println("--------testing opposite")
+  val baseSet = MySet(1, 3, 4, 5, 6)
+
+  val oppSet = !baseSet
+  println(baseSet(4))
+  println(oppSet(4))
+  println(oppSet(7))
+  val oppSet2 = oppSet.filter(_ % 2 == 0)
+  println(oppSet2(7))
+  val oppSet3 = oppSet2 + 7
+  println(oppSet3(7))
+
 
 }
 
