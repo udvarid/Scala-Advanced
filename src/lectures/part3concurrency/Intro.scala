73c739db6d5d028cc011c877cafeca8d3b7b2826
udvari.donat <udvarid@hotmail.com>
2020-03-29 21:33:22 +0200

    threads 2

--- src/lectures/part3concurrency/Intro.scala
+++ src/lectures/part3concurrency/Intro.scala
@@ -42,5 +42,48 @@ object Intro extends App {
   pool.shutdown() // => this will finish the ongoing threads
   //pool.shutdownNow() // this is kill the pool at this point
 
+  def runInParallel = {
+    var x = 0
+
+    val thread1 = new Thread(() => {
+      x = 1
+    })
+    val thread2 = new Thread(() => {
+      x = 2
+    })
+
+    thread1.start()
+    thread2.start()
+    if (x > 0) println(x) // only very rarely can the thread finish before this
+  }
+
+  for (_ <- 1 to 10000) runInParallel
+
+  class BankAccount(var amount: Int) {
+    override def toString: String = "" + amount
+  }
+
+  //def 1 : syncronization
+  def buySafe(account: BankAccount, thing: String, price: Int) = {
+    account.synchronized( {
+      account.amount -= price
+      println("I've bought " + thing)
+      println("My account is " + account)
+    })
+  }
+
+  //def 2: use @volatile before the variable which would be safe
+
+  //excercise
+  def inceptionThread(maxThreads: Int, index: Int = 1): Thread = new Thread(() => {
+    if (index < maxThreads ) {
+      val newThread = inceptionThread(maxThreads, index + 1)
+      newThread.start()
+      newThread.join()
+    }
+    println(s"This is the $index thread")
+  })
+
+  inceptionThread(50).start()
 
 }
